aa76c6e277be0446f85ea5a4d486e9b5a03d22e9
Donnie <im@kakajann.com>
2022-09-21 20:26:03 +0500

    feat: wishlist page

---
+++ src/routes/wishlist/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import ApartmentList from '$lib/components/ApartmentList/index.svelte';
+  import data from '$lib/apartments.json'
+  import { wishlist } from '$lib/stores'
+
+  $: apartments = data.filter((item) => $wishlist.includes(item.id))
+</script>
+
+<div id="wishlist">
+  <h2>My Wishlist</h2>
+
+  {#if apartments.length}
+    <ApartmentList {apartments} />
+  {:else}
+    <p>You have no apartment in your wishlist</p>
+  {/if}
+</div>
