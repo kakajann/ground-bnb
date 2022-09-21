8874c6c28d7a1026f08ac37b81f771512d82a6c4
Donnie <im@kakajann.com>
2022-09-21 17:47:50 +0500

    feat: create apartments list component

---
+++ src/lib/components/ApartmentList/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Apartment from "./Apartment.svelte";
+
+  export let apartments: App.Apartment.Item[] = []
+</script>
+
+<div id="apartments-list">
+  {#each apartments as apartment, index}
+    <Apartment {apartment} {index} />
+  {/each}
+</div>
