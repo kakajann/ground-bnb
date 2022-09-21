3b120c46f855f0ba73d144fc36c75e086b80cc5a
Donnie <im@kakajann.com>
2022-09-21 18:00:20 +0500

    feat: add menu to header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,6 +3,7 @@
     <img src="/logo.png" alt="ground bnb logo">
   </a>
   <div class="menu">
-    
+    <a href="/wishlist">My wishlist</a>
+    <a href="/wishlist" class="button">I'm a Host</a>
   </div>
 </header>
