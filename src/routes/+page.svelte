8db21096a6a0456a07d64ed75214910f9e66ef8f
Donnie <im@kakajann.com>
2022-09-21 17:48:10 +0500

    feat: use apartments list in home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
+<script>
+  import ApartmentList from '$lib/components/ApartmentList/index.svelte';
+  import data from '$lib/apartments.json'
+</script>
+
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<p>Dude</p>
+<div id="home">
+  <ApartmentList apartments={data} />
+  <div class="map">
+    map here
+  </div>
+</div>
