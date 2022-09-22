7a70244ce11501320a43bcfe5320760497f630e7
Donnie <im@kakajann.com>
2022-09-22 20:00:35 +0500

    feat: init create apartment form

---
+++ src/lib/components/CreateApartmentForm/index.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import ApartmentGenerator from "../ApartmentGenerator.svelte";
+</script>
+
+<form>
+  <div class="group">
+    <label for="">Title</label>
+    <input type="text" placeholder="Manhattan Bowery">
+  </div>
+  <div class="groups">
+    <div class="group">
+      <label for="">City</label>
+      <input type="text" placeholder="New York">
+    </div>
+    <div class="group">
+      <label for="">Country</label>
+      <input type="text" placeholder="USA">
+    </div>
+  </div>
+  <div class="groups">
+    <div class="group">
+      <label for="">Bedrooms</label>
+      <input type="text" placeholder="2">
+    </div>
+    <div class="group">
+      <label for="">Size (in square metre)</label>
+      <input type="text" placeholder="54">
+    </div>
+  </div>
+  <div class="group">
+    <label for="">Starting price</label>
+    <input type="text" placeholder="75">
+  </div>
+  <ApartmentGenerator />
+</form>
